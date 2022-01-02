a692d57a0ccb525982c23cf0592ee1ad101ddad2
Jay Butera <buterajay@protonmail.com>
2022-01-02 01:31:44 -0600

    Initial json record types

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,9 @@
 use std::path::PathBuf;
 use askama::Template;
 use structopt::StructOpt;
+use serde::{Serialize, Deserialize};
+
+pub type MediaUid = String;
 
 #[derive(Template)]
 #[template(path = "upload.html")]
@@ -26,4 +29,26 @@ pub struct Args {
     pub root_dir: PathBuf,
     #[structopt(short, long, default_value = "2342")]
     pub port: u32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TopicData {
+    /// Topic name
+    pub name: String,
+    /// Ordered list of media files
+    pub media: Vec<MediaUid>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq)]
+pub enum RevisionOp {
+    Add(Vec<MediaUid>),
+    Del(Vec<MediaUid>),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TopicRevisionHistory {
+    /// Topic name
+    pub topic: String,
+    // A stack of revisions, each revision is an ordered list of specific revision operations
+    pub revs: Vec<Vec<RevisionOp>>,
 }
