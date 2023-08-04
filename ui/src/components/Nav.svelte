34a8f7856ea3c4e2478876578e284a4247db9a8c
Jay Butera <buterajay@protonmail.com>
2023-08-04 18:49:39 -0500

    fix broken link

--- ui/src/components/Nav.svelte
+++ ui/src/components/Nav.svelte
@@ -1,7 +1,7 @@
 
 <div class="topnav">
     <a href="https://github.com/jaybutera/img">About</a>
-    <a href="/">New Collection</a>
+    <a href="/new-topic">New Collection</a>
     <a href="/new-index">New Index</a>
     <slot />
 </div>
